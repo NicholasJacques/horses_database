9e49497a7e8689d63d3ad947f4bf0e70cfe48346
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-03-16 22:37:30 -0600

    add horse feature

--- app/controllers/horse_app.rb
+++ app/controllers/horse_app.rb
@@ -1,6 +1,38 @@
 class HorseApp < Sinatra::Base
+
+  get '/' do
+    erb :"home/dashboard"
+  end
+
   get '/horses' do
     @horses = Horse.all
     erb :"horses/index"
   end
+
+  get '/horses/new' do
+    @horse = Horse.new
+    @jockeys = Jockey.all
+    @breeds = Breed.all
+    erb :"horses/new"
+  end
+
+  post '/horses' do
+    @horse = Horse.create(params[:horse])
+    redirect "/horses/#{@horse.id}"
+  end
+
+  get '/horses/:id' do
+    @horse = Horse.find(params[:id])
+    erb :"horses/show"
+  end
+
+  get '/jockeys' do
+    erb :"/jockeys/index"
+  end
+
+  get '/jockeys/:id' do
+    @jockey = Jockey.find(params[:id])
+    erb :"jockeys/show"
+  end
+
 end
